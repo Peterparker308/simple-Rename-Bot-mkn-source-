32447c25924a978f1e187ae09bb8803745a1340b
Peterparker308 <138222994+Peterparker308@users.noreply.github.com>
2023-07-19 17:33:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-ADMIN = int(environ.get("ADMIN", ""))          
-CAPTION = environ.get("CAPTION", "")
+API_ID = int(environ.get("API_ID", "29655234"))
+API_HASH = environ.get("API_HASH", "d4cd08a52d1d8c82226b487cc7d92f9c")
+BOT_TOKEN = environ.get("BOT_TOKEN", "6131243921:AAG24nZ2_tCsK_1baYF8h6YcPfePQ_HZfis")
+ADMIN = int(environ.get("ADMIN", "5363602465"))          
+CAPTION = environ.get("CAPTION", "@botupdates5")
 
 class temp(object):
-    THUMBNAIL = environ.get("THUMBNAIL", "")
+    THUMBNAIL = environ.get("THUMBNAIL", "https://te.legra.ph/file/e434394eabe3e897cede1.jpg")
